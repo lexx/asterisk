6c8f66fe6b43574f47fe1c1fd1a574af88272f90
lexx <54763126+lexx@users.noreply.github.com>
2019-09-01 17:36:51 +0400

    Update first_run.sh

--- first_run.sh
+++ first_run.sh
@@ -1,14 +1,14 @@
 yum -y update
-cp /usr/share/mc/syntax/sh.syntax /usr/share/mc/syntax/unknown.syntax
 mcedit /etc/sysconfig/selinux
 setenforce 0
+cp /usr/share/mc/syntax/sh.syntax /usr/share/mc/syntax/unknown.syntax
 
 mv /etc/localtime /etc/localtime.bak
 ln -s /usr/share/zoneinfo/Europe/Samara /etc/localtime
-yum install chrony
+yum -y install chrony
 systemctl start chronyd
 systemctl enable chronyd
 systemctl status chronyd
 
-yum -y install php-pear wget mc epel-release ncdu
+yum -y install php-pear mc epel-release dh ncdu net-tools
 date
